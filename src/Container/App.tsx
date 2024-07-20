998e4872956dc5e187a74ed5416e855418b9c2b1
Sasha Maksyutenko <sasha.maksyutenko@gmail.com>
2024-07-20 20:22:04 +0300

    Home Page

---
+++ src/Container/App.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Footer, Header } from "../Components/Layout";
+import { useState,useEffect } from "react";
+import { menuItemModel } from "../Interfaces";
+import { Home,MenuItemDetails,NotFound} from "../Pages";
+import { Route,Routes } from "react-router-dom";
+function App() {
+  
+  return (
+    <div>
+    <Header />
+    <div className="pb-5">
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/menuItemDetails/:menuItemId" element={<MenuItemDetails />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </div>
+    <Footer />
+  </div>
+  );
+}
+export default App;
---
+++ src/Pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MenuItemList from '../Components/Page/MenuItems/MenuItemList'
+function Home() {
+    return (
+        <div className='container p-2'>
+            <div>
+                <MenuItemList/>
+            </div>
+        </div>
+    )
+}
+export default Home
